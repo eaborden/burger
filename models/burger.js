8ff75c239712688328ce59eefe37c0a0287d6e9a
Edward Borden <arroll.borden@gmail.com>
2020-12-23 10:07:02 -0500

    burger model updated file

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,8 @@
 // Import the ORM to create functions that will interact with the database.
+//call back all of the orm
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
@@ -18,12 +19,12 @@ var cat = {
       cb(res);
     });
   },
-//   delete: function(condition, cb) {
-//     orm.delete("cats", condition, function(res) {
-//       cb(res);
-//     });
-//   }
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
