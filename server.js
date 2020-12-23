f4589eff535d38ce845622e66c1c24f31e4ae328
Edward Borden <arroll.borden@gmail.com>
2020-12-23 10:09:01 -0500

    server file updated

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
 var express = require("express");
 
+var app = express();
 var PORT = process.env.PORT || 8000;
 
-var app = express();
+
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
