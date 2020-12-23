19ee7c8e3a5f7a1260c537c36103008bf7171bc1
Edward Borden <arroll.borden@gmail.com>
2020-12-23 09:59:21 -0500

    final database schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 CREATE TABLE burgers 
 (
 id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(30),
+burger_name varchar(30) NOT NULL,
 devoured boolean default false,
 PRIMARY KEY (id)
 );
