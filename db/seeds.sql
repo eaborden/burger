f3d537e65e042430987a78643402454b64da5c66
Edward Borden <arroll.borden@gmail.com>
2020-12-23 10:00:12 -0500

    initial seed for the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('plant based', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('the works', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheesy cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('heart attack', false);
